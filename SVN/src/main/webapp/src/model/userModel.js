a3dbd3376a9cbf666a6054fc78b400a937b25326
lauraavila <la.avila30@uniandes.edu.co>
2014-08-13 09:41:24 -0500

    primer commit

---
+++ SVN/src/main/webapp/src/model/userModel.js
@@ -0,0 +1,10 @@
+App.Model.UserModel = Backbone.Model.extend({
+        defaults: {
+            'firstName': '',
+            'lastName': '',
+            'fecha':''
+        },
+        initialize: function() {
+ 
+        }
+    });
